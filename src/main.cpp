2c60a770d301aa7931c2fd41be9a5cff0f2dca44
JustinKin <justinkin2020@outlook.com>
2020-12-08 09:12:33 +0800

    Least Squares

--- src/main.cpp
+++ src/main.cpp
@@ -15,47 +15,49 @@ void LuSolve();
 
 int main()
 {
-	start = clock();
+    start = clock();
     //已知初始的5个坐标点
     float x[9] = {-386976.786, -397199.228, -403002.333, -404301.874, -401058.65, -393279.751, -381019.561, -364380.489, -343513.4};
-    float y[9] = {0.5027, -1.257, -2.915, -1.156, -2.815, 0, 0, 0, 0};
-    float z[9] = {0.5027, -1.257, -2.915, -1.156, -2.815, 0, 0, 0, 0};
+    float y[9] = {106369.212, 70584.989, 34008.818, -2946.251, -39859.487, -76305.83, -111859.429, -146097.317, -178603.222};
+    float z[9] = {61240.44, 41725.648, 21741.252, 1512.866, -18729.308, -38752.308, -58322.344, -77206.812, -95176.378};
 
-    float tmp = 2.0 * 12.0 / 96.0 ;
-    for(int i=0; i < 9; ++i)
+    float tmp = 2.0 * 12.0 / 96.0;
+    for (int i = 0; i < 9; ++i)
     {
         t[i] = tmp * i - 1;
     }
+    printf("=========== X坐标拟合结果 ===========\n");
     ComputeMatrix(x);
     GaussSolve();
+    printf("=========== Y坐标拟合结果 ===========\n");
     ComputeMatrix(y);
     LuSolve();
+    printf("=========== Z坐标拟合结果 ===========\n");
     ComputeMatrix(z);
     LuSolve();
-
 
     stop = clock();
     duration = (double)(stop - start) / CLK_TCK;
     printf("解方程所用时间：%f\n", duration);
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
 
 void ComputeMatrix(float *p)
 {
-    for(int i=0; i < 5; ++i)
+    for (int i = 0; i < 5; ++i)
     {
-        for(int j = i; j < 5; ++j)
+        for (int j = i; j < 5; ++j)
         {
-            for(int k = 0; k<9; ++k)
+            for (int k = 0; k < 9; ++k)
             {
                 A[i][j] += cos(i * acos(t[k])) * cos(j * acos(t[k]));
             }
             //对称性
-            A[j][i] = A [i][j];
+            A[j][i] = A[i][j];
         }
-        for(int k=0; k<9; ++k)
+        for (int k = 0; k < 9; ++k)
         {
             A[i][5] += p[k] * cos(i * acos(t[k]));
         }
@@ -87,7 +89,6 @@ void GaussSolve()
             }
         }
     }
-    printf("\n");
     //回代求解
     float result[5];
     result[4] = A[4][5] / A[4][4];
@@ -106,7 +107,7 @@ void GaussSolve()
     {
         printf("% 6f  ", result[i]);
     }
-    printf("\n");
+    printf("\n\n");
 }
 
 void LuSolve()
@@ -144,7 +145,6 @@ void LuSolve()
             }
         }
     }
-    printf("\n");
     //求解LY = B
     float Y[5];
     for (int i = 0; i < 5; ++i)
@@ -167,5 +167,4 @@ void LuSolve()
     for (int i = 0; i < 5; ++i)
         printf("% 6f  ", X[i]);
     printf("\n\n");
-
 }
